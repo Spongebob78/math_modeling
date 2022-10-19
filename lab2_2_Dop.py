943c43a10b6ddb81946b21e71cd32589fdf5dab8
Spongebob781 <94135164+Spongebob78@users.noreply.github.com>
2022-10-19 16:27:10 +0000

    wertfvb

--- lab2_2_Dop.py
+++ lab2_2_Dop.py
@@ -2,9 +2,9 @@ number0 = int(input())
 number1 = int(input())
 number2 = int(input())
 
-else:
-  if number0 == number1 == number2:
-    print("Треугольник равносторонний")
+if number0 == number1 == number2:
+  print("Треугольник равносторонний")
+if number0 + number1 < c or number1 + number2 < number0 or number0 + number2 < number1
   elif number0 != number1 != number2:
     print("Треугольник произвольный")
   elif number1 or number2 == number0:
