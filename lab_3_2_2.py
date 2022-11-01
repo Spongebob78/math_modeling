3e3b000de4d5bb81aef78d9b8d61ecb98a3608e4
Spongebob781 <94135164+Spongebob78@users.noreply.github.com>
2022-11-01 10:25:38 +0000

    efrv

---
+++ lab_3_2_2.py
@@ -0,0 +1,12 @@
+from master import e
+from master import h
+from master import k
+import numpy as np
+
+T = 200 
+E = 300
+
+N = (2 / (np.pi ** 0.5)) * (h / ((k * T) ** 1.5)) * (e ** (E / (k * T))) * (E ** (T / 2))
+
+print(N)
+              
---
+++ lab_3_2_dop.py
@@ -0,0 +1,18 @@
+from master import e
+from master import h
+from master import k
+import numpy as np
+T = 200 
+E = 300
+
+number1 = (2 / (np.pi ** 0.5))
+number2 = (h / ((k * T) ** (3 / 2)))
+number3 = (e ** (-E / (k * T))) # number3 = 0.0 = 
+number4 = (E ** (T / 2))
+N = number1 * number2 * number3 * number4
+print(N)
+
+
+
+
+
