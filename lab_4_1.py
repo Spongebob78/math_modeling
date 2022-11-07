dc9ce1c42aa0f1b90743a99264c0188bf6ff4322
Spongebob781 <94135164+Spongebob78@users.noreply.github.com>
2022-11-07 11:31:46 +0000

    wervtevf

---
+++ lab_4_1.py
@@ -0,0 +1,6 @@
+def number(a, b):
+  return (a + b) / 2
+
+a = int(input())
+b = int(input())
+print(number(a, b))
---
+++ lec_4_1.py
@@ -0,0 +1,6 @@
+def mult_funs(a):
+  x = 3 * a
+  return x 
+
+a = int(input())
+print(mult_funs(a))
