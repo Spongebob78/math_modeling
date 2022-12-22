9e44c49f2837b9fe0726f72b7f5e6d25de0b2c03
Spongebob78 <94135164+Spongebob78@users.noreply.github.com>
2022-12-22 16:33:58 +0000

    hmgdcx

--- lab_8.py
+++ lab_8.py
@@ -1,17 +1,31 @@
-import numpy as np
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import numpy as np
 
-def ellips(p = 10, ee = 0.9):
-    
+fig, ax = plt.subplots()
+ball, = plt.plot([], [], 'o', color='b')
+trajectory, = plt.plot([], [], '-', color='g')
+
+frames = np.arange(0, 4.85 * np.pi, 0.06)
+plt.axis('equal')
+ax.set_xlim(-1, 13)
+ax.set_ylim(0, 10)
+
+
+def ellips_fanc(p, ee): 
     fi = np.arange(0, np.pi * 8, 0.01)
-    
-    r = p / (1 + (ee * np.cos(fi)))
-    
+    r = 10 / (1 + (0.9 * np.cos(fi)))
     x = r * np.cos(fi)
     y = r * np.sin(fi)
-    plt.plot(x, y)
-    plt.grid()
-    plt.axis('equal')
-    plt.savefig('lab_6_dop_2.png')
+    return x, y
 
-ellips()
+X, Y = [], []
+
+def animate(i):
+    X.append(ellips_fanc(p = 10, ee = 0.9)[0])
+    Y.append(ellips_fanc()[1])
+    ball.set_data(ellips_fanc())
+    trajectory.set_data(X, Y)
+
+ani = animation.FuncAnimation(fig, animate, frames=frames, interval=30)
+ani.save('.gif')
--- primer_animate1.py
+++ primer_animate1.py
@@ -5,7 +5,6 @@ import numpy as np
 fig, ax = plt.subplots()
 ball, = plt.plot([], [], 'o', color='b')
 trajectory, = plt.plot([], [], '-', color='g')
-weels, = plt.plot([], [], '-', color='r')
 
 frames = np.arange(0, 4.85 * np.pi, 0.06)
 plt.axis('equal')
@@ -18,15 +17,6 @@ def circle_move(R, t):
     y = R * (1 - np.cos(t))
     return x, y
 
-def weels_fanc(R, x0, y0, vx0, vy0, t):
-    x0 = x0 + vx0 * t
-    y0 = y0 + vy0 * t
-
-    alpha = np.arange(0, 2*np.pi, 0.1)
-
-    x = x0 + R * np.cos(alpha)
-    y = y0 + R * np.sin(alpha)
-    return x, y
 
 X, Y = [], []
 R = 1 
@@ -36,7 +26,6 @@ def animate(i):
     Y.append(circle_move(R=R, t = i)[1])
     ball.set_data(circle_move(R=R, t=i))
     trajectory.set_data(X, Y)
-    weels.set_data(weels_fanc(R, 0, R, 1, 0, t=i))
 
 ani = animation.FuncAnimation(fig, animate, frames=frames, interval=30)
 ani.save('lab_7_dop_1.gif')
